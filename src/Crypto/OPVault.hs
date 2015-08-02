7cd6cc4776403f6a2d88f26df21085ebc1c1f928
Alex Crough <alex@crough.io>
2015-08-01 19:59:30 -0700

    Clearing the OPVault library
    Getting ready for writing an actual application to handle encryption. Needed exported functions will be moved to OPVault.hs as needed.

--- src/Crypto/OPVault.hs
+++ src/Crypto/OPVault.hs
@@ -1,18 +1,3 @@
-{-# LANGUAGE LambdaCase        #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE RankNTypes        #-}
 module Crypto.OPVault where
 
-import Crypto.OPVault.Encryption
-import Crypto.OPVault.FileSystem
-import Crypto.OPVault.Types
 
-result :: ResultT IO ()
-result = undefined
-
-main' :: IO ()
-main' = runResultT result>>= \x ->
-    case x of
-      Left err -> putStr "ERROR:" >> putStrLn err
-      Right () -> putStrLn "\nDone!"
